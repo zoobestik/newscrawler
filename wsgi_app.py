2e527016a092e95885abcb459ac5236c249cf81f
kb.chernenko <kb.chernenko@gmail.com>
2011-09-15 09:36:40 +0000

    Add inpex page with list support RSS

--- wsgi_app.py
+++ wsgi_app.py
@@ -1,9 +1,15 @@
 # coding: utf-8
 import sys, os
-from bottle import response, route, debug, default_app
+from bottle import response, route, debug, default_app, template
 from parse import get_feed_parser_by_id
+from lib.config import get_config_data
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+@route('/:is_page_exist#(index\.html)?#', template='index')
+def index(is_page_exist):
+	urls = get_config_data()['urls']
+	return dict(urls=urls, )
 
 @route('/id:id#[0-9]+#')
 def generate_feed(id):
